8643c06441313c93bb801ba34178122897b8993a
Taranjot Singh <tr548284@dal.ca>
2023-11-21 01:34:34 -0400

    feat: ✨ added the AssignmentViewModel for fetching assignment details from firebase

---
+++ app/src/main/java/com/example/dalportal/ui/assignments/AssignmentViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.dalportal.ui.assignments
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.dalportal.model.Assignment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class AssignmentViewModel : ViewModel() {
+    private val _assignments = MutableLiveData<List<Assignment>>()
+    private val db: FirebaseFirestore = Firebase.firestore
+
+    init {
+        // Fetch assignments from Firestore
+        fetchAssignments()
+    }
+
+    private fun fetchAssignments() {
+        db.collection("assignments")
+            .addSnapshotListener { querySnapshot, exception ->
+                if (exception != null) {
+                    handleQueryFailure(exception)
+                    return@addSnapshotListener
+                }
+
+                if (querySnapshot != null) {
+                    handleQuerySuccess(querySnapshot)
+                }
+            }
+    }
+
+
+    private fun handleQuerySuccess(querySnapshot: QuerySnapshot) {
+        val assignmentsList = mutableListOf<Assignment>()
+
+        for (document in querySnapshot.documents) {
+            val assignment = document.toObject(Assignment::class.java)
+            assignment?.let { assignmentsList.add(it) }
+        }
+
+        _assignments.value = assignmentsList
+    }
+
+    private fun handleQueryFailure(exception: Exception) {
+        // Handle error here
+    }
+
+    val assignments: LiveData<List<Assignment>> = _assignments
+}
