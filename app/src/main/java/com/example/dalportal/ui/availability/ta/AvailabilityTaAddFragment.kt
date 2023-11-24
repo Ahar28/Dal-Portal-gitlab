c5b6638e26070bad6b9c2fe9f421028627b53cc0
shubhamc <sh572302@dal.ca>
2023-11-23 20:17:50 -0400

    added database sync

--- app/src/main/java/com/example/dalportal/ui/availability/ta/AvailabilityTaAddFragment.kt
+++ app/src/main/java/com/example/dalportal/ui/availability/ta/AvailabilityTaAddFragment.kt
@@ -3,16 +3,26 @@ package com.example.dalportal.ui.availability.ta
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.dalportal.R
+import com.example.dalportal.ui.availability.firebase.AvailabilityData
+import com.example.dalportal.ui.availability.firebase.ButtonPair
+import com.example.dalportal.ui.availability.firebase.convertButtonPairsToStrings
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+
 
 class AvailabilityTaAddFragment : Fragment() {
 
@@ -24,11 +34,15 @@ class AvailabilityTaAddFragment : Fragment() {
     private lateinit var linearLayoutInnerTimeRanges: LinearLayout
     private lateinit var spinnerDays: Spinner
     private lateinit var spinnerAdapter: ArrayAdapter<String>
+    private lateinit var databaseReference: DatabaseReference
+    val db = FirebaseFirestore.getInstance()
+    val userId = 101
 
-    private val timeRangeButtonsMap = mutableMapOf<String, MutableList<Pair<Button, Button>>>()
+    private var timeRangeButtonsMap = mutableMapOf<String, MutableList<Pair<Button, Button>>>()
 
     private var selectedStartDate = getCurrentDate()
     private var selectedEndDate = getCurrentDate()
+    private var comment: String = ""
 
     private fun initializeTimeRangeButtonsMap() {
         val daysOfWeek = arrayOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
@@ -38,12 +52,25 @@ class AvailabilityTaAddFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        loadDataFromFirestore(db, userId.toString())
+    }
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_availability_ta_add, container, false)
+
+        // Initialize Firebase
+        // Initialize Firebase storage
+        databaseReference = Firebase.database.reference
+
 
         initializeTimeRangeButtonsMap()
 
@@ -132,8 +159,134 @@ class AvailabilityTaAddFragment : Fragment() {
         // Section 4
         etComment = view.findViewById(R.id.etComment)
 
+        val updateButton: Button = view.findViewById(R.id.btnSubmitAvability)
+
+        updateButton.setOnClickListener(){
+            saveDataToFirebase(db)
+        }
+
         return view
     }
+
+    private fun saveDataToFirebase(db: FirebaseFirestore ) {
+        // Create a unique key for the availability data
+//        val availabilityKey = databaseReference.child("availability").push().key
+        val convertedTimeRangeButtonsMap = convertButtonPairsToStrings(timeRangeButtonsMap)
+
+        // Create a data object to be saved
+        val availabilityData = AvailabilityData(
+            userId.toString(),
+            convertedTimeRangeButtonsMap,
+            selectedStartDate,
+            selectedEndDate,
+            etComment.text.toString()
+        )
+
+        db.collection("availability")
+            .document(userId.toString())
+            .set(availabilityData)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${userId}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+    }
+
+    private fun loadDataFromFirestore(db: FirebaseFirestore, documentId: String) {
+        val docRef = db.collection("availability").document(documentId)
+
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    val availabilityData = document.toObject(AvailabilityData::class.java)
+                    // Process the retrieved data (e.g., update UI)
+                    updateUI(availabilityData)
+                } else {
+                    // Document does not exist
+                    // Handle the case accordingly
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle failures
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+    }
+
+    private fun updateUI(availabilityData: AvailabilityData?) {
+        // Update your UI with the retrieved data
+        if (availabilityData != null) {
+            selectedStartDate = availabilityData.selectedStartDate
+            selectedEndDate = availabilityData.selectedEndDate
+            timeRangeButtonsMap = convertStringPairsToButtons(availabilityData.timeRangeButtonsMap)
+            etComment.setText(availabilityData.comment)
+
+
+            for (buttonPair in timeRangeButtonsMap["Sun"]!!) {
+                // Create a LinearLayout to wrap the buttons
+                val timeRangeLayout = LinearLayout(requireContext())
+                timeRangeLayout.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                timeRangeLayout.orientation = LinearLayout.HORIZONTAL
+
+                // Create a TextView between the buttons
+                val timeRangeSeparator = TextView(requireContext())
+                timeRangeSeparator.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                timeRangeSeparator.text = " - "
+
+                val parentButton1Layout = buttonPair.first.parent as? ViewGroup
+                parentButton1Layout?.removeView(buttonPair.first)
+
+                val parentButton2Layout = buttonPair.second.parent as? ViewGroup
+                parentButton2Layout?.removeView(buttonPair.second)
+                // Add views to the layout
+                timeRangeLayout.addView(buttonPair.first)
+                timeRangeLayout.addView(timeRangeSeparator)
+                timeRangeLayout.addView(buttonPair.second)
+
+                // Add the layout to the parent layout
+                linearLayoutInnerTimeRanges.addView(timeRangeLayout)
+            }
+
+        }
+    }
+
+    fun convertStringPairsToButtons(buttonPairs: Map<String, List<ButtonPair>>): MutableMap<String, MutableList<Pair<Button, Button>>> {
+        return buttonPairs.mapValues { (day, pairs) ->
+            pairs.map { pair ->
+                val btnStartTime = Button(requireContext())
+                btnStartTime.layoutParams = LinearLayout.LayoutParams(
+                    0,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1.0f
+                )
+                btnStartTime.text = pair.first
+                btnStartTime.setOnClickListener {
+                    showTimePickerDialog(btnStartTime, day)
+                }
+
+                val btnEndTime = Button(requireContext())
+                btnEndTime.layoutParams = LinearLayout.LayoutParams(
+                    0,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1.0f
+                )
+                btnEndTime.text = pair.second
+                btnEndTime.setOnClickListener {
+                    showTimePickerDialog(btnEndTime, day)
+                }
+
+                Pair(btnStartTime, btnEndTime)
+            }.toMutableList()
+        }.toMutableMap()
+    }
+
 
     private fun showDatePickerDialog(targetButton: Button) {
         val calendar = Calendar.getInstance()
