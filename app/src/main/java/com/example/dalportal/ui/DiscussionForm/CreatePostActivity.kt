66b6929fc4a147d2dacd7bdbab6ccb60844849cf
slakhanpal <sh475218@dal.ca>
2023-11-25 08:59:04 -0400

    Admin : Added changes for Admin dashboard to only show up when user logged in and boundary cases for discussion form

--- app/src/main/java/com/example/dalportal/ui/DiscussionForm/CreatePostActivity.kt
+++ app/src/main/java/com/example/dalportal/ui/DiscussionForm/CreatePostActivity.kt
@@ -34,7 +34,7 @@ class CreatePostActivity : AppCompatActivity() {
             user = "Username", // Replace with actual user identifier
             content = content
         )
-
+    if(!newPost.content.trim().isNullOrEmpty()) {
         FirestoreHelper.addPost(newPost, onSuccess = {
             // Handle success
             Toast.makeText(this, "Post added successfully", Toast.LENGTH_SHORT).show()
@@ -43,6 +43,10 @@ class CreatePostActivity : AppCompatActivity() {
             // Handle failure
             Toast.makeText(this, "Error adding post: ${e.message}", Toast.LENGTH_SHORT).show()
         })
+    }
+        else{
+        Toast.makeText(this, "You cannot submit an empty discussion post.", Toast.LENGTH_SHORT).show()
+    }
     }
 
 }
