649e96bf3b5a518a160e99f4c74770c5cf913a15
Tapan Patel <tapan.patel@dal.ca>
2023-11-25 20:14:45 -0400

    added job update feature

--- app/src/main/java/com/example/dalportal/ui/job_portal/job_listing/JobDetailsFragment.kt
+++ app/src/main/java/com/example/dalportal/ui/job_portal/job_listing/JobDetailsFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dalportal.R
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.dalportal.databinding.FragmentJobDetailsBinding
 import com.example.dalportal.model.JobListing
+import com.google.firebase.firestore.FirebaseFirestore
 
 class JobDetailsFragment : Fragment() {
     private var _binding: FragmentJobDetailsBinding? = null
     private val binding get() = _binding!!
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,15 +28,53 @@ class JobDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val jobListing: JobListing? = arguments?.getParcelable("jobListing")
         displayJobDetails(jobListing)
+
+        binding.updateButton.setOnClickListener {
+            updateJobDetails(jobListing)
+        }
     }
 
     private fun displayJobDetails(jobListing: JobListing?) {
         jobListing?.let {
-            // Bind the job details to your UI components here
-            binding.textViewJobTitle.text = it.title
-            binding.textViewJobDescription.text = it.description
-            binding.textViewJobLocation.text = it.location
-            binding.textViewJobPay.text = it.pay.toString()
+            binding.editTextJobTitle.setText(it.title)
+            binding.editTextJobDescription.setText(it.description)
+            binding.editTextJobLocation.setText(it.location)
+            binding.editTextJobPay.setText(it.pay.toString())
+            binding.editTextJobPosition.setText(it.positions.toString())
+            binding.editTextJobRequirement.setText(it.requirements)
+            binding.editTextJobType.setText(it.type)
+        }
+    }
+
+    private fun updateJobDetails(jobListing: JobListing?) {
+        jobListing?.let { job ->
+            val updatedTitle = binding.editTextJobTitle.text.toString()
+            val updatedDescription = binding.editTextJobDescription.text.toString()
+            val updatedLocation = binding.editTextJobLocation.text.toString()
+            val updatedPay = binding.editTextJobPay.text.toString().toIntOrNull() ?: 0
+            val updatedPositions = binding.editTextJobPosition.text.toString().toIntOrNull() ?: 0
+            val updatedRequirements = binding.editTextJobRequirement.text.toString()
+            val updatedType = binding.editTextJobType.text.toString()
+
+            val updatedJobData: Map<String, Any> = hashMapOf(
+                "title" to updatedTitle,
+                "description" to updatedDescription,
+                "location" to updatedLocation,
+                "pay" to updatedPay,
+                "positions" to updatedPositions,
+                "requirements" to updatedRequirements,
+                "type" to updatedType
+            )
+
+            db.collection("jobPostings").document(job.id)
+                .update(updatedJobData)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Job updated successfully", Toast.LENGTH_SHORT).show()
+                    findNavController().navigateUp()  // Navigate back to the previous fragment
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(context, "Error updating job: ${e.message}", Toast.LENGTH_LONG).show()
+                }
         }
     }
 
