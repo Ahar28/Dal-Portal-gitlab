9ef178018736ee10b4b4ba07f79edaa36687341b
Dheeraj Bhat <dh210086@dal.ca>
2023-11-26 13:22:08 -0400

    Fixed scrolling issue in chat

---
+++ app/src/main/java/com/example/dalportal/util/DashItems.kt
@@ -0,0 +1,32 @@
+package com.example.dalportal.util
+
+object DashItems {
+    operator fun get(role: String?): List<String> {
+        return when (role) {
+            "TA" -> taItems
+            "Professor" -> professorItems
+            "admin" -> adminItems
+            else -> studentItems
+        }
+    }
+
+    private val studentItems: List<String> = arrayListOf("assignments_submit", "events", "forum")
+    private val taItems: List<String> =
+        arrayListOf(
+            "ta_portal",
+            "update_availability",
+            "assignments_review",
+            "content",
+            "events",
+            "forum"
+        )
+    private val professorItems: List<String> = arrayListOf(
+        "prof_portal",
+        "review_availability",
+        "assignments_review",
+        "content",
+        "events",
+        "forum"
+    )
+    private val adminItems: List<String> = arrayListOf("admin_portal", "events", "forum")
+}
